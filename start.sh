06df7fd2e86579825a1017833f737da1beaed06f
=revertingcode <revertingcode@gmail.com>
2025-12-04 18:42:21 +0100

    HA for Youjazz

---
+++ start.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]; then
+    echo "Specify the instance to run: 1 or 2"
+    exit 1
+fi
+
+# istance to run
+DIR="$1"
+
+#stop both instances
+pm2 stop yj-$DIR
+
+#rebuild
+npm run clean
+npm run build
+
+#startYoujazz in HA
+pm2 start dist-$DIR/index.js --name "yj-$DIR"
